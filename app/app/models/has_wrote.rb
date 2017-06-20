974e6963299d843fdc095252fd85981ffb0ecc4e
Rohan Challa <rchalla769@gmail.com>
2017-06-20 16:48:15 -0700

    Added Relationships

---
+++ app/app/models/has_wrote.rb
@@ -0,0 +1,9 @@
+class HasWrote
+  include Neo4j::ActiveRel
+
+  from_class :Person
+  to_class :Movie
+  type :WROTE
+  creates_unique
+
+end
--- app/app/models/movie.rb
+++ app/app/models/movie.rb
@@ -1,8 +1,9 @@
-class Movie 
+class Movie
   include Neo4j::ActiveNode
   property :title, type: String
   property :released, type: Integer
   property :tagline, type: String
+
 
 
 
